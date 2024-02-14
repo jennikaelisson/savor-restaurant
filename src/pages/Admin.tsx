e211ab8ee917b341f0cb51a5cc3c0ff3f50273c2
David Törnros <david@dxgruppen.se>
2024-02-14 21:47:12 +0100

    added adminstate with localstorage

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -1,11 +1,37 @@
 import Login from "../components/Login";
+import { useState } from "react";
 
 const Admin = () => {
+	const [admin, setAdmin] = useState(
+		() => localStorage.getItem("admin") !== null
+	);
+	const handleAdminStatus = (adminStatus: boolean) => {
+		// Depending on adminStatus value, set or remove from local storage
+		adminStatus
+			? localStorage.setItem("admin", "true")
+			: localStorage.removeItem("admin");
+		// Update the state
+		setAdmin(adminStatus);
+	};
+
 	return (
 		<>
 			<h1>Admin</h1>
-
-			<Login />
+			{admin ? (
+				<>
+					<div className="p-2 bg-light border my-2">
+						Adminpanel should be here
+					</div>
+					<button
+						className="btn btn-outline-primary"
+						onClick={() => handleAdminStatus(false)}
+					>
+						Logout
+					</button>
+				</>
+			) : (
+				<Login handleAdminStatus={handleAdminStatus} />
+			)}
 		</>
 	);
 };

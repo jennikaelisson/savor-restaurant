65890e17648cc13ba069b6abf290649b94f629b4
David Törnros <david@dxgruppen.se>
2024-02-15 20:01:54 +0100

    customer info

--- src/pages/Book.tsx
+++ src/pages/Book.tsx
@@ -7,31 +7,31 @@ import {
 
 import { useState, useEffect } from "react";
 
-type DatePiece = Date | null;
-type DateValue = DatePiece | [DatePiece, DatePiece];
-
 const Book = () => {
 	const [restaurantBookings, setRestaurantBookings] = useState<any>(null);
-	const [selectedDate, changeDate] = useState<DateValue>(new Date());
+	const [selectedDate, changeDate] = useState<Date>(new Date());
 	const [selectedGuests, changeGuests] = useState(2);
-	const [selectedTime, changeTime] = useState("18:00");
+	const [selectedTime, changeTime] = useState("");
+
+	const [freeTables, setFreeTables] = useState<{ [key: string]: number }>({
+		"18:00": 15,
+		"21:00": 15,
+	});
+	const [customer, setCustomer] = useState({
+		name: "testar",
+		lastname: "testar",
+		email: "someone@somedomain.com",
+		phone: "070-1112233",
+	});
+
+	const handleCustomerChange = (field: string, value: string) => {
+		setCustomer((prevCustomer) => ({ ...prevCustomer, [field]: value }));
+	};
 
 	useEffect(() => {
-		const freeTables: { [key: string]: number } = {
-			"18:00": 15,
-			"21:00": 15,
-		};
-		restaurantBookings?.forEach((element: Object) => {
-			if (element.date == formatDate(selectedDate)) {
-				const numberOfTables = Math.floor((element.numberOfGuests - 1) / 6) + 1;
-				freeTables[element.time] -= numberOfTables;
-			}
-		});
-
-		let tablesNeeded = Math.floor((selectedGuests - 1) / 6) + 1;
-		console.log(`Needed (${tablesNeeded}) `);
-		console.log(freeTables);
-	}, [selectedGuests, selectedDate, selectedTime]);
+		updateFreeTables();
+		changeTime("");
+	}, [selectedGuests, selectedDate]);
 
 	useEffect(() => {
 		fetchData();
@@ -48,6 +48,40 @@ const Book = () => {
 		return `${year}-${month}-${day}`;
 	};
 
+	const updateFreeTables = () => {
+		const updatedFreeTables = {
+			"18:00": 15,
+			"21:00": 15,
+		};
+
+		restaurantBookings?.forEach((element: any) => {
+			if (element.date === formatDate(selectedDate)) {
+				const numberOfTables = Math.floor((element.numberOfGuests - 1) / 6) + 1;
+				updatedFreeTables[element.time] -= numberOfTables;
+			}
+		});
+
+		setFreeTables(updatedFreeTables);
+	};
+	const createNewBooking = async () => {
+		const bookingData = {
+			restaurantId: "65cc7dddf65c4399fb07d036",
+			date: formatDate(selectedDate),
+			time: selectedTime,
+			numberOfGuests: selectedGuests,
+			customer: {
+				name: "testaa",
+				lastname: "testaa",
+				email: "someone@somedomain.com",
+				phone: "070-1112233",
+			},
+		};
+		await createBookingService(bookingData);
+		fetchData();
+	};
+
+	const tablesNeeded = Math.floor((selectedGuests - 1) / 6) + 1;
+
 	return (
 		<>
 			<p className="h1">Booking</p>
@@ -57,7 +91,7 @@ const Book = () => {
 				<input
 					type="number"
 					min="1"
-					max="240"
+					max="90"
 					value={selectedGuests}
 					onChange={(e) => changeGuests(parseInt(e.target.value))}
 				/>
@@ -77,32 +111,84 @@ const Book = () => {
 				<input
 					type="radio"
 					className="btn-check"
-					name="options-outlined"
-					id="success-outlined"
+					id="time18"
 					checked={selectedTime === "18:00"}
 					onChange={() => changeTime("18:00")}
+					disabled={freeTables["18:00"] < tablesNeeded}
 				/>
 				<label
-					className="btn btn-outline-success mx-2"
-					htmlFor="success-outlined"
+					className={`btn ${
+						freeTables["18:00"] < tablesNeeded
+							? "btn-danger"
+							: "btn-outline-success"
+					} mx-2`}
+					htmlFor="time18"
 				>
-					18:00
+					18:00 ({freeTables["18:00"]} available)
 				</label>
 
 				<input
 					type="radio"
 					className="btn-check"
-					name="options-outlined"
-					id="danger-outlined"
+					id="time21"
 					checked={selectedTime === "21:00"}
 					onChange={() => changeTime("21:00")}
+					disabled={freeTables["21:00"] < tablesNeeded}
 				/>
 				<label
-					className="btn btn-outline-success mx-2"
-					htmlFor="danger-outlined"
+					className={`btn ${
+						freeTables["21:00"] < tablesNeeded
+							? "btn-danger"
+							: "btn-outline-success"
+					} mx-2`}
+					htmlFor="time21"
 				>
-					21:00
+					21:00 ({freeTables["21:00"]} available)
 				</label>
+			</div>
+			<p className="h2">Your information</p>
+			<div className="bg-light border p-2">
+				<label htmlFor="name">Name:</label>
+				<input
+					type="text"
+					id="name"
+					value={customer.name}
+					onChange={(e) => handleCustomerChange("name", e.target.value)}
+				/>
+
+				<label htmlFor="lastname">Lastname:</label>
+				<input
+					type="text"
+					id="lastname"
+					value={customer.lastname}
+					onChange={(e) => handleCustomerChange("lastname", e.target.value)}
+				/>
+
+				<label htmlFor="email">Email:</label>
+				<input
+					type="email"
+					id="email"
+					value={customer.email}
+					onChange={(e) => handleCustomerChange("email", e.target.value)}
+				/>
+
+				<label htmlFor="phone">Phone:</label>
+				<input
+					type="tel"
+					id="phone"
+					value={customer.phone}
+					onChange={(e) => handleCustomerChange("phone", e.target.value)}
+				/>
+			</div>
+			<p className="h2">Book</p>
+			<div className="bg-light border p-2">
+				<button
+					className="btn btn-success"
+					onClick={createNewBooking}
+					disabled={!selectedTime}
+				>
+					Book table
+				</button>
 			</div>
 		</>
 	);

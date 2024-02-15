2ffc47e19afe5af249380a4c51b2a1ba7596b342
David Törnros <david@dxgruppen.se>
2024-02-15 20:10:43 +0100

    confirmpage

--- src/pages/Book.tsx
+++ src/pages/Book.tsx
@@ -12,7 +12,7 @@ const Book = () => {
 	const [selectedDate, changeDate] = useState<Date>(new Date());
 	const [selectedGuests, changeGuests] = useState(2);
 	const [selectedTime, changeTime] = useState("");
-
+	const [bookingConfirmed, setBookingConfirmed] = useState(false);
 	const [freeTables, setFreeTables] = useState<{ [key: string]: number }>({
 		"18:00": 15,
 		"21:00": 15,
@@ -77,7 +77,7 @@ const Book = () => {
 			},
 		};
 		await createBookingService(bookingData);
-		fetchData();
+		setBookingConfirmed(true);
 	};
 
 	const tablesNeeded = Math.floor((selectedGuests - 1) / 6) + 1;
@@ -85,111 +85,126 @@ const Book = () => {
 	return (
 		<>
 			<p className="h1">Booking</p>
+			{bookingConfirmed ? (
+				<div className="mt-3 alert alert-success" role="alert">
+					Booking confirmed! Thank you, {customer.name} {customer.lastname}, for
+					your reservation. <br />
+					<br />
+					Date: {formatDate(selectedDate)}
+					<br />
+					Time: {selectedTime}
+					<br />
+					Guests: {selectedGuests}
+					<br />
+				</div>
+			) : (
+				<>
+					<p className="h2">Number of guests</p>
+					<div className="bg-light border p-2">
+						<input
+							type="number"
+							min="1"
+							max="90"
+							value={selectedGuests}
+							onChange={(e) => changeGuests(parseInt(e.target.value))}
+						/>
+					</div>
+					<p className="h2">Select a date</p>
+					<div className="bg-light border p-2">
+						<Calendar
+							onChange={changeDate}
+							value={selectedDate}
+							minDate={new Date()}
+							maxDate={new Date(new Date().setMonth(new Date().getMonth() + 1))}
+						/>
+					</div>
 
-			<p className="h2">Number of guests</p>
-			<div className="bg-light border p-2">
-				<input
-					type="number"
-					min="1"
-					max="90"
-					value={selectedGuests}
-					onChange={(e) => changeGuests(parseInt(e.target.value))}
-				/>
-			</div>
-			<p className="h2">Select a date</p>
-			<div className="bg-light border p-2">
-				<Calendar
-					onChange={changeDate}
-					value={selectedDate}
-					minDate={new Date()}
-					maxDate={new Date(new Date().setMonth(new Date().getMonth() + 1))}
-				/>
-			</div>
+					<p className="h2">Select a time</p>
+					<div className="bg-light border p-2">
+						<input
+							type="radio"
+							className="btn-check"
+							id="time18"
+							checked={selectedTime === "18:00"}
+							onChange={() => changeTime("18:00")}
+							disabled={freeTables["18:00"] < tablesNeeded}
+						/>
+						<label
+							className={`btn ${
+								freeTables["18:00"] < tablesNeeded
+									? "btn-danger"
+									: "btn-outline-success"
+							} mx-2`}
+							htmlFor="time18"
+						>
+							18:00 ({freeTables["18:00"]} available)
+						</label>
 
-			<p className="h2">Select a time</p>
-			<div className="bg-light border p-2">
-				<input
-					type="radio"
-					className="btn-check"
-					id="time18"
-					checked={selectedTime === "18:00"}
-					onChange={() => changeTime("18:00")}
-					disabled={freeTables["18:00"] < tablesNeeded}
-				/>
-				<label
-					className={`btn ${
-						freeTables["18:00"] < tablesNeeded
-							? "btn-danger"
-							: "btn-outline-success"
-					} mx-2`}
-					htmlFor="time18"
-				>
-					18:00 ({freeTables["18:00"]} available)
-				</label>
+						<input
+							type="radio"
+							className="btn-check"
+							id="time21"
+							checked={selectedTime === "21:00"}
+							onChange={() => changeTime("21:00")}
+							disabled={freeTables["21:00"] < tablesNeeded}
+						/>
+						<label
+							className={`btn ${
+								freeTables["21:00"] < tablesNeeded
+									? "btn-danger"
+									: "btn-outline-success"
+							} mx-2`}
+							htmlFor="time21"
+						>
+							21:00 ({freeTables["21:00"]} available)
+						</label>
+					</div>
+					<p className="h2">Your information</p>
+					<div className="bg-light border p-2">
+						<label htmlFor="name">Name:</label>
+						<input
+							type="text"
+							id="name"
+							value={customer.name}
+							onChange={(e) => handleCustomerChange("name", e.target.value)}
+						/>
 
-				<input
-					type="radio"
-					className="btn-check"
-					id="time21"
-					checked={selectedTime === "21:00"}
-					onChange={() => changeTime("21:00")}
-					disabled={freeTables["21:00"] < tablesNeeded}
-				/>
-				<label
-					className={`btn ${
-						freeTables["21:00"] < tablesNeeded
-							? "btn-danger"
-							: "btn-outline-success"
-					} mx-2`}
-					htmlFor="time21"
-				>
-					21:00 ({freeTables["21:00"]} available)
-				</label>
-			</div>
-			<p className="h2">Your information</p>
-			<div className="bg-light border p-2">
-				<label htmlFor="name">Name:</label>
-				<input
-					type="text"
-					id="name"
-					value={customer.name}
-					onChange={(e) => handleCustomerChange("name", e.target.value)}
-				/>
+						<label htmlFor="lastname">Lastname:</label>
+						<input
+							type="text"
+							id="lastname"
+							value={customer.lastname}
+							onChange={(e) => handleCustomerChange("lastname", e.target.value)}
+						/>
 
-				<label htmlFor="lastname">Lastname:</label>
-				<input
-					type="text"
-					id="lastname"
-					value={customer.lastname}
-					onChange={(e) => handleCustomerChange("lastname", e.target.value)}
-				/>
+						<label htmlFor="email">Email:</label>
+						<input
+							type="email"
+							id="email"
+							value={customer.email}
+							onChange={(e) => handleCustomerChange("email", e.target.value)}
+						/>
 
-				<label htmlFor="email">Email:</label>
-				<input
-					type="email"
-					id="email"
-					value={customer.email}
-					onChange={(e) => handleCustomerChange("email", e.target.value)}
-				/>
-
-				<label htmlFor="phone">Phone:</label>
-				<input
-					type="tel"
-					id="phone"
-					value={customer.phone}
-					onChange={(e) => handleCustomerChange("phone", e.target.value)}
-				/>
-			</div>
-			<p className="h2">Book</p>
-			<div className="bg-light border p-2">
-				<button
-					className="btn btn-success"
-					onClick={createNewBooking}
-					disabled={!selectedTime}
-				>
-					Book table
-				</button>
-			</div>
+						<label htmlFor="phone">Phone:</label>
+						<input
+							type="tel"
+							id="phone"
+							value={customer.phone}
+							onChange={(e) => handleCustomerChange("phone", e.target.value)}
+						/>
+					</div>
+					<p className="h2">Book</p>
+					<div className="bg-light border p-2">
+						<button
+							className="btn btn-success"
+							onClick={createNewBooking}
+							disabled={!selectedTime}
+						>
+							Book table
+						</button>
+					</div>
+				</>
+			)}
 		</>
 	);
 };

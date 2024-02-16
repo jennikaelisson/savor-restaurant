32ea43999bd2cf98cac44bd35784c24cca844b67
Jennika Elisson <jennika.elisson@gmail.com>
2024-02-16 13:37:25 +0100

    cleaned up admin page with booking button

---
+++ src/components/AdminCreateBooking.tsx
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+import Book from '../pages/Book';
+
+export const AdminCreateBooking = () => {
+  const [isBookVisible, setBookVisibility] = useState(false);
+  const [isButtonVisible, setButtonVisibility] = useState(true);
+
+  const handleButtonClick = () => {
+    setBookVisibility(true);
+    setButtonVisibility(false);
+  };
+
+  return (
+    <>
+      {isButtonVisible && (
+        <button className="btn btn-outline-primary" onClick={handleButtonClick}>
+          Make a new booking
+        </button>
+      )}
+      {isBookVisible && <Book />}
+    </>
+  );
+};

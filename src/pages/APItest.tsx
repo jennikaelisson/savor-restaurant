2cf9246a049e91f4756c5944b3a261e365e498da
David Törnros <david@dxgruppen.se>
2024-02-16 17:32:18 +0100

    clear code, add edit customer base

--- src/pages/APItest.tsx
+++ src/pages/APItest.tsx
@@ -1,76 +1,63 @@
 import {
 	getBookingsService,
-	getRestaurantDataService,
-	createBookingService,
-	updateBookingService,
 	getCustomerDataService,
+	updateBookingService,
+	updateCustomerService,
 	deleteBookingService,
 } from "../services/bookingService.ts";
 import { useState, useEffect } from "react";
 
 const APItest = () => {
-	const [restaurantData, setRestaurantData] = useState<any>(null);
-	const [restaurantBookings, setRestaurantBookings] = useState<any>(null);
-	const [customerInformations, setCustomerInformation] = useState<{
+	const [bookings, setBookings] = useState<any>(null);
+	const [customers, setCustomers] = useState<{
 		[key: string]: any;
 	}>({});
-	const [bookingFormData, setBookingFormData] = useState<string>("");
+
 	const [updatedBookingValues, setUpdatedBookingValues] = useState<{
+		[key: string]: any;
+	}>({});
+	const [updatedCustomerValues, setUpdatedCustomerValues] = useState<{
 		[key: string]: any;
 	}>({});
 
 	const fetchData = async () => {
-		setRestaurantData(await getRestaurantDataService());
-		setRestaurantBookings(await getBookingsService());
+		setBookings(await getBookingsService());
 	};
 
 	const fetchCustomerData = async (customerId: string) => {
 		const fetchedCustomerInfo = await getCustomerDataService(customerId);
 		console.log(fetchedCustomerInfo);
 
-		setCustomerInformation((prevCustomerInfo) => ({
+		setCustomers((prevCustomerInfo) => ({
 			...prevCustomerInfo,
 			[customerId]: fetchedCustomerInfo[0],
 		}));
 	};
 
-	const createNewBooking = async () => {
-		const bookingData = JSON.parse(bookingFormData);
-		await createBookingService(bookingData);
-		fetchData();
-	};
-
 	const updateBooking = async (bookingID: string) => {
-		console.log("Update");
+		console.log("Update booking");
 		console.log(updatedBookingValues[bookingID]);
 		await updateBookingService(bookingID, updatedBookingValues[bookingID]);
 	};
+
+	const updateCustomer = async (customerID: string) => {
+		console.log("Update customer");
+		console.log(updatedBookingValues[customerID]);
+		await updateCustomerService(customerID, updatedCustomerValues[customerID]);
+	};
+
 	const deleteBooking = async (bookingID: string) => {
 		await deleteBookingService(bookingID);
 		fetchData();
 	};
 
 	useEffect(() => {
-		setBookingFormData(
-			JSON.stringify({
-				restaurantId: "65cc7dddf65c4399fb07d036",
-				date: "2022-03-21",
-				time: "18:00",
-				numberOfGuests: 4,
-				customer: {
-					name: "Franzén",
-					lastname: "Sebastian",
-					email: "someone@somedomain.com",
-					phone: "070-1112233",
-				},
-			})
-		);
 		fetchData();
 	}, []);
 
 	useEffect(() => {
 		const initialUpdatedValues: { [key: string]: any } = {};
-		restaurantBookings?.forEach((booking: any) => {
+		bookings?.forEach((booking: any) => {
 			initialUpdatedValues[booking._id] = {
 				date: booking.date,
 				time: booking.time,
@@ -80,7 +67,7 @@ const APItest = () => {
 			fetchCustomerData(booking.customerId);
 		});
 		setUpdatedBookingValues(initialUpdatedValues);
-	}, [restaurantBookings]);
+	}, [bookings]);
 
 	const handleBookingUpdateInputChange = (
 		bookingID: string,
@@ -100,90 +87,56 @@ const APItest = () => {
 		<>
 			<h1>API test</h1>
 
-			<b>GET Restaurant:</b>
 			<div className="bg-light border">
-				{restaurantData && restaurantData.length > 0 ? (
+				{bookings && bookings.length > 0 ? (
 					<>
-						<h2>{restaurantData[0].name}</h2>
-						<p>Address: {restaurantData[0].address}</p>
-						<p>Zip Code: {restaurantData[0].zip}</p>
-						<p>City: {restaurantData[0].city}</p>
-					</>
-				) : (
-					<div className="spinner-border text-primary" role="status">
-						<span className="visually-hidden">Loading...</span>
-					</div>
-				)}
-			</div>
-
-			<b>Create booking:</b>
-			<div className="bg-light border">
-				<textarea
-					className="form-control"
-					value={bookingFormData}
-					onChange={(e) => setBookingFormData(e.target.value)}
-				/>
-				<button className="btn btn-success" onClick={createNewBooking}>
-					CREATE
-				</button>
-			</div>
-
-			<b>GET Bookings:</b>
-			<div className="bg-light border">
-				{restaurantBookings && restaurantBookings.length > 0 ? (
-					<>
-						<h2>Bookings</h2>
-						{restaurantBookings.map((booking: any) => (
+						{bookings.map((booking: any) => (
 							<div key={booking._id} className="bg-primary my-2 p-2">
 								<p>ID: {booking._id}</p>
-								<p>
-									Date:
-									<input
-										type="text"
-										value={updatedBookingValues[booking._id]?.date || ""}
-										onChange={(e) =>
-											handleBookingUpdateInputChange(
-												booking._id,
-												"date",
-												e.target.value
-											)
-										}
-									/>
-								</p>
-								<p>
-									Time:
-									<input
-										type="text"
-										value={updatedBookingValues[booking._id]?.time || ""}
-										onChange={(e) =>
-											handleBookingUpdateInputChange(
-												booking._id,
-												"time",
-												e.target.value
-											)
-										}
-									/>
-								</p>
-								<p>
-									Number of Guests:
-									<input
-										type="number"
-										value={
-											updatedBookingValues[booking._id]?.numberOfGuests || ""
-										}
-										onChange={(e) =>
-											handleBookingUpdateInputChange(
-												booking._id,
-												"numberOfGuests",
-												parseInt(e.target.value, 10)
-											)
-										}
-										min="1"
-										max="240"
-									/>
-								</p>
-
-								<div className="bg-secondary p-2">
+								Date:
+								<input
+									type="text"
+									value={updatedBookingValues[booking._id]?.date || ""}
+									onChange={(e) =>
+										handleBookingUpdateInputChange(
+											booking._id,
+											"date",
+											e.target.value
+										)
+									}
+								/>
+								<br />
+								Time:
+								<input
+									type="text"
+									value={updatedBookingValues[booking._id]?.time || ""}
+									onChange={(e) =>
+										handleBookingUpdateInputChange(
+											booking._id,
+											"time",
+											e.target.value
+										)
+									}
+								/>
+								<br />
+								Number of Guests:
+								<input
+									type="number"
+									value={
+										updatedBookingValues[booking._id]?.numberOfGuests || ""
+									}
+									onChange={(e) =>
+										handleBookingUpdateInputChange(
+											booking._id,
+											"numberOfGuests",
+											parseInt(e.target.value, 10)
+										)
+									}
+									min="1"
+									max="240"
+								/>
+								<br />
+								<div className="bg-secondary p-2 my-2">
 									<p>
 										CustomerID:
 										<input
@@ -203,23 +156,19 @@ const APItest = () => {
 									</p>
 									<hr />
 									<p>
-										Name: {customerInformations[booking.customerId]?.name || ""}{" "}
-										{customerInformations[booking.customerId]?.lastname || ""}
+										Name: {customers[booking.customerId]?.name || ""}{" "}
+										{customers[booking.customerId]?.lastname || ""}
 									</p>
-									<p>
-										Email:{" "}
-										{customerInformations[booking.customerId]?.email || ""}
-									</p>
-									<p>
-										Phone:{" "}
-										{customerInformations[booking.customerId]?.phone || ""}
-									</p>
+									<p>Email: {customers[booking.customerId]?.email || ""}</p>
+									<p>Phone: {customers[booking.customerId]?.phone || ""}</p>
 								</div>
-
 								<div>
 									<button
 										className="btn btn-warning"
-										onClick={() => updateBooking(booking._id)}
+										onClick={() => {
+											updateBooking(booking._id);
+											updateCustomer(booking.customerId);
+										}}
 									>
 										Update
 									</button>

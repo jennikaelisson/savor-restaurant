687481c75e195cadc9037ed1e71cfb8cd75037ec
Jennika Elisson <jennika.elisson@gmail.com>
2024-02-18 20:18:31 +0100

    Added useLocation to condition classes in Layout

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,20 +1,25 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 const Layout = () => {
+	
+	const location = useLocation();
+
+	const isHome = location.pathname === '/'; // Adjust the path accordingly
+  
 	return (
-		<>
-			<Header />
-
-			<main className="flex-shrink-0">
-				<div>
-					<Outlet />
-				</div>
-			</main>
-
-			<Footer />
-		</>
+	  <>
+		<Header />
+  
+		<main className={` ${isHome ? 'flex-shrink' : 'flex-shrink-0 mt-5'}`}>
+		  <div className={` ${isHome ? '' : 'container py-4'}`}>
+			<Outlet />
+		  </div>
+		</main>
+  
+		<Footer />
+	  </>
 	);
-};
+  };
 
 export default Layout;

6438bdc4910e6f3d5b1d789ac1570babbc822672
Jennika Elisson <jennika.elisson@gmail.com>
2024-02-19 22:42:59 +0100

    removed old code

--- src/components/AdminCreateBooking.tsx
+++ src/components/AdminCreateBooking.tsx
@@ -1,23 +1,12 @@
-import { useState } from 'react';
+
 import Book from '../pages/Book';
 
 export const AdminCreateBooking = () => {
-  const [isBookVisible, setBookVisibility] = useState(false);
-  const [isButtonVisible, setButtonVisibility] = useState(true);
-
-  const handleButtonClick = () => {
-    setBookVisibility(true);
-    setButtonVisibility(false);
-  };
+  
 
   return (
     <>
-      {isButtonVisible && (
-        <button className="btn btn-outline-primary" onClick={handleButtonClick}>
-          Make a new booking
-        </button>
-      )}
-      {isBookVisible && <Book />}
+      <Book />
 
      
     </>

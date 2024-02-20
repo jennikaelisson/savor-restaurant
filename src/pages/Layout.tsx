24168cdf2483ccbd8127c2c5231c00324c5e661c
David Törnros <david@dxgruppen.se>
2024-02-20 10:23:17 +0100

    Hook to scroll page to top on location change

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,25 +1,29 @@
 import { Outlet, useLocation } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import { useEffect } from "react";
 const Layout = () => {
-	
+	useEffect(() => {
+		window.scrollTo(0, 0);
+	}, [useLocation()]);
+
 	const location = useLocation();
 
-	const isHome = location.pathname === '/' || location.pathname === '/book'; // Adjust the path accordingly
-  
+	const isHome = location.pathname === "/" || location.pathname === "/book"; // Adjust the path accordingly
+
 	return (
-	  <>
-		<Header />
-  
-		<main className={` ${isHome ? 'flex-shrink' : 'flex-shrink-0 mt-5'}`}>
-		  <div className={` ${isHome ? '' : 'container py-4'}`}>
-			<Outlet />
-		  </div>
-		</main>
-  
-		<Footer />
-	  </>
+		<>
+			<Header />
+
+			<main className={` ${isHome ? "flex-shrink" : "flex-shrink-0 mt-5"}`}>
+				<div className={` ${isHome ? "" : "container py-4"}`}>
+					<Outlet />
+				</div>
+			</main>
+
+			<Footer />
+		</>
 	);
-  };
+};
 
 export default Layout;

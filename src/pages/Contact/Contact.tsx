502fd28ff04c1502cfd65b44003897342686916c
Suhyeon Cho <suhyeon2196@gmail.com>
2024-02-21 15:52:57 +0100

    add img to contact page

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -1,28 +1,39 @@
 import ContactForm from "./ContactForm";
 
-
 const Contact = () => {
-	return (
-		<main className="flex-shrink mt-5">
-			<div className="container py-4">
-				<h1 className='h1'>CONTACT US</h1>
-				<section>
-					<p>
-						To get in touch with the Savor team simply fill out the contact form
-						below. <br />
-						Alternatively you can telephone us on 079 123 4567 or email us at{" "}
-						<br />
-						contact@savor.se <br />
-						<br />
-						Address: Drottninggatan 17, Stockholm Sweden
-					</p>
-					<div>
-						<ContactForm />
-					</div>
-				</section>
-			</div>
-		</main>
-	);
+    return (
+        <main className="flex-shrink pt-5">
+            <div className="container-fluid">
+                <div className="row justify-content-center">
+                    <div className="col-12 col-md-6 p-5">
+                        <h1 className='h1'>CONTACT US</h1>
+                        <section>
+                            <p>
+                                To get in touch with the Savor team simply fill out the contact form
+                                below. <br />
+                                Alternatively you can telephone us on 079 123 4567 or email us at{" "}
+                                <br />
+                                contact@savor.se <br />
+                                <br />
+                                Address: Drottninggatan 17, Stockholm Sweden
+                            </p>
+                            <div>
+                                <ContactForm />
+                            </div>
+                        </section>
+                    </div>
+                
+                    <div className="col-12 col-md-6 d-none d-md-block" style={{ maxHeight: "800px", overflow: "hidden" }}>
+                        <img
+                            src="src/assets/ContactPic.jpg"
+                            alt="A made up table"
+                            className="img-fluid fade-in-container img-scale" style={{ width: "100%", height: "100%", objectFit: "cover"}}
+                        />
+                    </div>
+                </div>
+            </div>
+        </main>
+    );
 };
 
 export default Contact;

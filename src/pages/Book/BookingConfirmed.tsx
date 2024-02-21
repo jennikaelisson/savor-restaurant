1681710a5d23f9677054b1dd9476b6d69d36a8ec
David Törnros <david@dxgruppen.se>
2024-02-21 23:25:19 +0100

    seperate component

--- src/pages/Book/BookingConfirmed.tsx
+++ src/pages/Book/BookingConfirmed.tsx
@@ -8,16 +8,18 @@ interface BookingConfirmedProps {
 const BookingConfirmed = (props: BookingConfirmedProps) => {
 	const { name, date, time, guests } = props;
 	return (
-		<div className="mt-3 alert alert-success" role="alert">
-			Booking confirmed! Thank you, {name}, for your reservation. <br />
-			<br />
-			Date: {date}
-			<br />
-			Time: {time}
-			<br />
-			Guests: {guests}
-			<br />
-		</div>
+		<>
+			<div className="mt-3 alert alert-success" role="alert">
+				Booking confirmed! Thank you, {name}, for your reservation. <br />
+				<br />
+				Date: {date}
+				<br />
+				Time: {time}
+				<br />
+				Guests: {guests}
+				<br />
+			</div>
+		</>
 	);
 };
 

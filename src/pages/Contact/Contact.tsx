1bc3857bbcc07d56e723eb5a70dcb1033ef4a662
Suhyeon Cho <suhyeon2196@gmail.com>
2024-02-21 15:08:15 +0100

    add className to h1

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -1,10 +1,11 @@
 import ContactForm from "./ContactForm";
 
+
 const Contact = () => {
 	return (
 		<main className="flex-shrink mt-5">
 			<div className="container py-4">
-				<h1>CONTACT US</h1>
+				<h1 className='h1'>CONTACT US</h1>
 				<section>
 					<p>
 						To get in touch with the Savor team simply fill out the contact form

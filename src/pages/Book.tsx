fac46fc99b993f50ea68f83ee0721a158986c6ad
David Törnros <david@dxgruppen.se>
2024-02-21 09:00:34 +0100

    update

--- src/pages/Book.tsx
+++ src/pages/Book.tsx
@@ -85,198 +85,200 @@ const Book = () => {
 	const tablesNeeded = Math.floor((selectedGuests - 1) / 6) + 1;
 
 	return (
-		<div className="row">
-			<div
-				className=" p-5 col-12 col-md-6"
-				style={{ height: "100vh", overflowY: "auto" }}
-			>
-				<p className="h1 pt-5">BOOKING</p>
-				{bookingConfirmed ? (
-					<div className="mt-3 alert alert-success" role="alert">
-						Booking confirmed! Thank you, {customer.name} {customer.lastname},
-						for your reservation. <br />
-						<br />
-						Date: {formatDate(selectedDate)}
-						<br />
-						Time: {selectedTime}
-						<br />
-						Guests: {selectedGuests}
-						<br />
-					</div>
-				) : (
-					<>
-						<div>
-							{" "}
-							<p className="h4">Number of guests</p>
-							<div className=" p-2 pb-4">
-								<input
-									type="number"
-									min="1"
-									max="90"
-									className="form-control input-border"
-									value={selectedGuests}
-									onChange={(e) => changeGuests(parseInt(e.target.value))}
+		<main className="flex-shrink">
+			<div className="row">
+				<div
+					className=" p-5 col-12 col-md-6"
+					style={{ height: "100vh", overflowY: "auto" }}
+				>
+					<p className="h1 pt-5">BOOKING</p>
+					{bookingConfirmed ? (
+						<div className="mt-3 alert alert-success" role="alert">
+							Booking confirmed! Thank you, {customer.name} {customer.lastname},
+							for your reservation. <br />
+							<br />
+							Date: {formatDate(selectedDate)}
+							<br />
+							Time: {selectedTime}
+							<br />
+							Guests: {selectedGuests}
+							<br />
+						</div>
+					) : (
+						<>
+							<div>
+								{" "}
+								<p className="h4">Number of guests</p>
+								<div className=" p-2 pb-4">
+									<input
+										type="number"
+										min="1"
+										max="90"
+										className="form-control input-border"
+										value={selectedGuests}
+										onChange={(e) => changeGuests(parseInt(e.target.value))}
+									/>
+								</div>
+							</div>
+							<p className="h4">Select a date</p>
+							<div className="p-2 pb-4">
+								<Calendar
+									onChange={setSelectedDate}
+									value={selectedDate}
+									minDate={new Date()}
+									maxDate={
+										new Date(new Date().setMonth(new Date().getMonth() + 1))
+									}
 								/>
 							</div>
-						</div>
-						<p className="h4">Select a date</p>
-						<div className="p-2 pb-4">
-							<Calendar
-								onChange={setSelectedDate}
-								value={selectedDate}
-								minDate={new Date()}
-								maxDate={
-									new Date(new Date().setMonth(new Date().getMonth() + 1))
-								}
-							/>
-						</div>
 
-						<p className="h4">Select a time</p>
-						<div className="p-2 pb-4">
-							<input
-								type="radio"
-								className="btn-check"
-								id="time18"
-								checked={selectedTime === "18:00"}
-								onChange={() => changeTime("18:00")}
-								disabled={freeTables["18:00"] < tablesNeeded}
-							/>
-							<label
-								className={`btn ${
-									freeTables["18:00"] < tablesNeeded
-										? "btn-danger"
-										: "btn-outline-dark button"
-								} mx-2`}
-								htmlFor="time18"
-							>
-								18:00 ({freeTables["18:00"]} available)
-							</label>
-
-							<input
-								type="radio"
-								className="btn-check"
-								id="time21"
-								checked={selectedTime === "21:00"}
-								onChange={() => changeTime("21:00")}
-								disabled={freeTables["21:00"] < tablesNeeded}
-							/>
-							<label
-								className={`btn ${
-									freeTables["21:00"] < tablesNeeded
-										? "btn-danger"
-										: "btn-outline-dark button"
-								} mx-2`}
-								htmlFor="time21"
-							>
-								21:00 ({freeTables["21:00"]} available)
-							</label>
-						</div>
-						<p className="h4">Your information</p>
-						<div className="p-2 pr-4">
-							<div className="row mb-3">
+							<p className="h4">Select a time</p>
+							<div className="p-2 pb-4">
+								<input
+									type="radio"
+									className="btn-check"
+									id="time18"
+									checked={selectedTime === "18:00"}
+									onChange={() => changeTime("18:00")}
+									disabled={freeTables["18:00"] < tablesNeeded}
+								/>
 								<label
-									htmlFor="name"
-									className="col-12 col-sm-4 col-form-label"
+									className={`btn ${
+										freeTables["18:00"] < tablesNeeded
+											? "btn-danger"
+											: "btn-outline-dark button"
+									} mx-2`}
+									htmlFor="time18"
 								>
-									Name:
+									18:00 ({freeTables["18:00"]} available)
 								</label>
-								<div className="col-12 col-sm-8">
-									<input
-										type="text"
-										id="name"
-										className="form-control input-border"
-										value={customer.name}
-										onChange={(e) =>
-											handleCustomerChange("name", e.target.value)
-										}
-									/>
-								</div>
+
+								<input
+									type="radio"
+									className="btn-check"
+									id="time21"
+									checked={selectedTime === "21:00"}
+									onChange={() => changeTime("21:00")}
+									disabled={freeTables["21:00"] < tablesNeeded}
+								/>
+								<label
+									className={`btn ${
+										freeTables["21:00"] < tablesNeeded
+											? "btn-danger"
+											: "btn-outline-dark button"
+									} mx-2`}
+									htmlFor="time21"
+								>
+									21:00 ({freeTables["21:00"]} available)
+								</label>
 							</div>
-							<div className="row mb-3">
-								<label
-									htmlFor="lastname"
-									className="col-12 col-sm-4 col-form-label"
-								>
-									Last name:
-								</label>
-								<div className="col-12 col-sm-8">
-									<input
-										type="text"
-										id="lastname"
-										className="form-control input-border"
-										value={customer.lastname}
-										onChange={(e) =>
-											handleCustomerChange("lastname", e.target.value)
-										}
-									/>
+							<p className="h4">Your information</p>
+							<div className="p-2 pr-4">
+								<div className="row mb-3">
+									<label
+										htmlFor="name"
+										className="col-12 col-sm-4 col-form-label"
+									>
+										Name:
+									</label>
+									<div className="col-12 col-sm-8">
+										<input
+											type="text"
+											id="name"
+											className="form-control input-border"
+											value={customer.name}
+											onChange={(e) =>
+												handleCustomerChange("name", e.target.value)
+											}
+										/>
+									</div>
 								</div>
-							</div>
+								<div className="row mb-3">
+									<label
+										htmlFor="lastname"
+										className="col-12 col-sm-4 col-form-label"
+									>
+										Last name:
+									</label>
+									<div className="col-12 col-sm-8">
+										<input
+											type="text"
+											id="lastname"
+											className="form-control input-border"
+											value={customer.lastname}
+											onChange={(e) =>
+												handleCustomerChange("lastname", e.target.value)
+											}
+										/>
+									</div>
+								</div>
 
-							<div className="row mb-3">
-								{" "}
-								<label
-									htmlFor="email"
-									className="col-12 col-sm-4 col-form-label"
-								>
-									Email:
-								</label>
-								<div className="col-12 col-sm-8">
+								<div className="row mb-3">
 									{" "}
-									<input
-										type="email"
-										id="email"
-										className="form-control input-border"
-										value={customer.email}
-										onChange={(e) =>
-											handleCustomerChange("email", e.target.value)
-										}
-									/>
+									<label
+										htmlFor="email"
+										className="col-12 col-sm-4 col-form-label"
+									>
+										Email:
+									</label>
+									<div className="col-12 col-sm-8">
+										{" "}
+										<input
+											type="email"
+											id="email"
+											className="form-control input-border"
+											value={customer.email}
+											onChange={(e) =>
+												handleCustomerChange("email", e.target.value)
+											}
+										/>
+									</div>
 								</div>
-							</div>
 
-							<div className="row mb-3">
-								{" "}
-								<label
-									htmlFor="phone"
-									className="col-12 col-sm-4 col-form-label"
-								>
-									Phone:
-								</label>
-								<div className="col-12 col-sm-8">
+								<div className="row mb-3">
 									{" "}
-									<input
-										type="tel"
-										id="phone"
-										className="form-control input-border"
-										value={customer.phone}
-										onChange={(e) =>
-											handleCustomerChange("phone", e.target.value)
-										}
-									/>
+									<label
+										htmlFor="phone"
+										className="col-12 col-sm-4 col-form-label"
+									>
+										Phone:
+									</label>
+									<div className="col-12 col-sm-8">
+										{" "}
+										<input
+											type="tel"
+											id="phone"
+											className="form-control input-border"
+											value={customer.phone}
+											onChange={(e) =>
+												handleCustomerChange("phone", e.target.value)
+											}
+										/>
+									</div>
 								</div>
 							</div>
-						</div>
 
-						<div className="border p-2">
-							<button
-								className="btn button"
-								onClick={createNewBooking}
-								disabled={!selectedTime}
-							>
-								BOOK TABLE
-							</button>
-						</div>
-					</>
-				)}
+							<div className="border p-2">
+								<button
+									className="btn button"
+									onClick={createNewBooking}
+									disabled={!selectedTime}
+								>
+									BOOK TABLE
+								</button>
+							</div>
+						</>
+					)}
+				</div>
+				<div className="col-12 col-md-6">
+					<img
+						src="src/img/Table.jpg"
+						alt="A made up table"
+						className="img-fluid fade-in-container img-scale"
+					/>
+				</div>
 			</div>
-			<div className="col-12 col-md-6">
-				<img
-					src="src/img/Table.jpg"
-					alt="A made up table"
-					className="img-fluid fade-in-container img-scale"
-				/>
-			</div>
-		</div>
+		</main>
 	);
 };
 
